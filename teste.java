bef6064d1fdc153c4575dd9e092334345419a515
Kevin <kevinsum73@gmail.com>
2024-08-02 20:23:55 -0300

    novos prints

--- teste.java
+++ teste.java
@@ -1,5 +1,6 @@
 public class teste{
     public static void main(String[] args) {
-        System.out.println("Ola mundo!");
+        System.out.println("LMAO!");
+        System.out.println("Ola mundo.");
     }
 }
